bb9522f850a29d41175ee7dc6837d5580e802d53
K.T. TU <kttu@media.ee.ntu.edu.tw>
2025-08-15 20:31:49 +0800

    Modify inference.sh

--- HW/Final/inference.sh
+++ HW/Final/inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEVICE=$1
-MODE=$2  # patch 或 seg
+MODE=$2  # 'patch' or 'seg' or keep empty
 
 CMD="CUDA_VISIBLE_DEVICES=${DEVICE} \
     python3 ./src/run.py \
@@ -10,45 +10,20 @@ CMD="CUDA_VISIBLE_DEVICES=${DEVICE} \
 
 if [ "$MODE" = "patch" ]; then
     CMD="$CMD --patch \
-        --output './src/lamp_patch.txt'"
+        --output './src/score/lamp_patch.txt'"
 elif [ "$MODE" = "seg" ]; then
     CMD="$CMD --segmentation \
-        --output './src/lamp_segmentation.txt'"
+        --output './src/score/lamp_segmentation.txt'"
 else
-    CMD="$CMD --output './src/lamp_origin.txt'"
+    CMD="$CMD --output './src/score/lamp_origin.txt'"
 fi
 
 eval "$CMD"
 
 if [ "$MODE" = "patch" ]; then
-    python3 ./src/eval.py --input './src/lamp_patch.txt'
+    python3 ./src/eval.py --input './src/score/lamp_patch.txt'
 elif [ "$MODE" = "seg" ]; then
-    python3 ./src/eval.py --input './src/lamp_segmentation.txt'
+    python3 ./src/eval.py --input './src/score/lamp_segmentation.txt'
 else
-    python3 ./src/eval.py --input './src/lamp_origin.txt'
+    python3 ./src/eval.py --input './src/score/lamp_origin.txt'
 fi
-
-
-
-# CUDA_VISIBLE_DEVICES=$1 \
-# python3 ./src/run.py \
-# --input './input_list/list_CASIA-Iris-Lamp.txt' \
-# --output './src/lamp_origin.txt' \
-# --checkpoint_file_path './src/custom/ckpt_weights_Lamp'
-
-# CUDA_VISIBLE_DEVICES=$1 \
-# python3 ./src/run.py \
-# --input './input_list/list_CASIA-Iris-Lamp.txt' \
-# --output './src/lamp_patch.txt' \
-# --checkpoint_file_path './src/custom/ckpt_weights_Lamp' \
-# --patch
-
-# CUDA_VISIBLE_DEVICES=$1 \
-# python3 ./src/run.py \
-# --input './input_list/list_CASIA-Iris-Lamp.txt' \
-# --checkpoint_file_path './src/custom/ckpt_weights_Lamp' \
-# --segmentation
-
-# python3 ./src/eval.py --input './src/score/lamp_origin.txt'
-# python3 ./src/eval.py --input './src/score/lamp_patch.txt'
-# python3 ./src/eval.py --input './src/score/lamp_segmentation.txt'
